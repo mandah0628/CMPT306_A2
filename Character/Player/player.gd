c9c4e6349a21fd762d5dfdab188830a85510f760
Matt Tsogt <mandah0628@gmail.com>
2024-10-04 13:11:12 -0600

    added script for moving backward and forward. max speed is limited

--- Character/Player/player.gd
+++ Character/Player/player.gd
@@ -1,10 +1,34 @@
 extends CharacterBody2D
 
-var rotation_speed = 200.0
+var rotation_speed = 10.0
+var max_speed = 500.0
+var acceleration = 275.0
+var ship_velocity = Vector2.ZERO
 
-func _movementProcess(delta):
-	if Input.is_action_pressed("ui_left"):
+func _process(delta):
+	#Controls for turning left
+	if Input.is_action_pressed("strafe_left"):
 		rotation-=rotation_speed*delta
 	
-	if Input.is_action_pressed(delta):
+	#Controls for turning right
+	if Input.is_action_pressed("strafe_right"):
 		rotation+=rotation_speed*delta
+		
+	#Controls for moving forward
+	if Input.is_action_pressed("move_forward"):
+		var direction = Vector2(cos(rotation), sin(rotation))  
+		ship_velocity += direction * acceleration * delta
+		
+	#Controls for moving backward
+	if Input.is_action_pressed("move_backward"):
+		var direction = Vector2(cos(rotation), sin(rotation))
+		ship_velocity -= direction * acceleration * delta
+		
+	#Limits the max speed
+	if ship_velocity.length() > max_speed:
+		ship_velocity = ship_velocity.normalized() * max_speed
+		
+	#Changes the ship position
+	position += ship_velocity * delta
+		
+		
