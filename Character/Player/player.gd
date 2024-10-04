50493309a25a348caf6e45465b2087b4a3b004e3
Matt Tsogt <mandah0628@gmail.com>
2024-10-04 13:32:04 -0600

    changed the function for deaccelerating the ship when no key is being pressed by using if conditions

--- Character/Player/player.gd
+++ Character/Player/player.gd
@@ -4,7 +4,7 @@ var rotation_speed = 10.0
 var max_speed = 500.0
 var acceleration = 300.0
 var ship_velocity = Vector2.ZERO
-var deacceleration = 0.95
+var deacceleration = 100
 
 func _process(delta):
 	#Controls for turning left
@@ -25,8 +25,28 @@ func _process(delta):
 		var direction = Vector2(cos(rotation), sin(rotation))
 		ship_velocity -= direction * acceleration * delta
 	
+	#Deaccelerates the ship if the movement keys are not pressed
 	if not Input.is_action_pressed("move_backward") and not Input.is_action_pressed("move_forward"):
-		ship_velocity *= pow(deacceleration,delta)
+		var velocity_length = ship_velocity.length()
+		
+		#if ship is in motion, reduce speed(reduces the magnitue of the vector)
+		if velocity_length > 0:
+			velocity_length -= deacceleration * delta
+		
+		#if ship is not in motion
+		if velocity_length < 0:
+			velocity_length = 0
+		
+		#if ship is in motion, apply the new reduced speed
+		#by multuplying the new magnitued with the normal vector of the
+		#direction of where the ship is going
+		if velocity_length > 0:
+			ship_velocity = ship_velocity.normalized() * velocity_length
+			
+		#if not, comes to complete stop
+		else:
+			ship_velocity = Vector2.ZERO
+			
 	#Limits the max speed
 	if ship_velocity.length() > max_speed:
 		ship_velocity = ship_velocity.normalized() * max_speed
