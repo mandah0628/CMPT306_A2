8909c251633dd6176ddb54338ef0ce5890e462db
Matt Tsogt <mandah0628@gmail.com>
2024-10-04 13:50:15 -0600

    added a function that deletes the laser object one it is off screen, to preserve memory

--- Scripts/laser.gd
+++ Scripts/laser.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
+var movement_vector := Vector2(0, -1)
+var speed = 500.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta):
+	global_position += movement_vector * speed * delta
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
