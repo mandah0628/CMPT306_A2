e4bbca2465125fc3e5e4bc558e9b481dc6416dfd
Matt Tsogt <mandah0628@gmail.com>
2024-10-04 13:16:20 -0600

    added function for deaccelerating the ship when no backward and movement is pressed. needs debuggin, as ship is not stopping

--- Character/Player/player.gd
+++ Character/Player/player.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 var rotation_speed = 10.0
 var max_speed = 500.0
-var acceleration = 275.0
+var acceleration = 300.0
 var ship_velocity = Vector2.ZERO
+var deacceleration = 0.95
 
 func _process(delta):
 	#Controls for turning left
@@ -23,7 +24,9 @@ func _process(delta):
 	if Input.is_action_pressed("move_backward"):
 		var direction = Vector2(cos(rotation), sin(rotation))
 		ship_velocity -= direction * acceleration * delta
-		
+	
+	if not Input.is_action_pressed("move_backward") and not Input.is_action_pressed("move_forward"):
+		ship_velocity *= pow(deacceleration,delta)
 	#Limits the max speed
 	if ship_velocity.length() > max_speed:
 		ship_velocity = ship_velocity.normalized() * max_speed
